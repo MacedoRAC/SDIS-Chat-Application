87401065f2e0fa09de6292aa22b14f56c13aecbc
Mário Macedo <andr.macedo1@gmail.com>
2015-06-05 22:48:39 +0100

    roomContoller structure

--- src/GUI/RoomController.java
+++ src/GUI/RoomController.java
@@ -1,11 +1,37 @@
 package GUI;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 
 /**
  * Created by André on 05/06/2015.
  */
 public class RoomController {
 
+    @FXML
+    private ListView<String> peopleOnChat;
 
+    @FXML
+    private Button addPeopleBtn;
+
+    @FXML
+    private ListView<String> msgsContainer;
+
+    @FXML
+    private TextArea msgField;
+
+    @FXML
+    private Button sendBtn;
+
+    @FXML
+    private void send(){
+
+    }
+
+    @FXML
+    private void addPeopleToRoom(){
+
+    }
 }
